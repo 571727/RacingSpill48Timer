66dd263f72cbd42b5d97ac98b38f0d6844f926c6
jhoffis <31273598+jhoffis@users.noreply.github.com>
2019-08-29 08:17:59 +0200

    Added remaining variables and added JavaDoc to ease further use.

--- RacingSpill48Timer/src/elem/CarRep.java
+++ RacingSpill48Timer/src/elem/CarRep.java
@@ -1,7 +1,16 @@
 package elem;
 
-public class CarRepresentation {
-	
+/**
+ * Used to store stats about Car and more easily communicate Cars with the
+ * server. Also used to restore a players car after they have lost connection
+ * and rejoined.
+ * 
+ * @author jonah
+ *
+ */
+
+public class CarRep {
+
 	private String name;
 	private int nosTimeStandard;
 	private int nosBottleAmountStandard;
@@ -9,15 +18,39 @@ public class CarRepresentation {
 	private double hp;
 	private double currentWeight;
 	private double topSpeed;
+	private int idleSpeed;
 	private int totalRPM;
 	private int totalGear;
 	private boolean upgradedGears;
+	private int tireGripTimeStandard;
 	private double tireGripStrengthStandard;
+	private double tireGripAreaTop;
+	private double tireGripAreaBottom;
 	private int[] upgradeLVLs;
 
-	public CarRepresentation(String name, int nosTimeStandard, int nosBottleAmountStandard, double nosStrengthStandard,
-			double hp, double currentWeight, double topSpeed, int totalRPM, int totalGear, boolean upgradedGears,
-			double tireGripStrengthStandard, int[] upgradeLVLs) {
+	/**
+	 * @param name                     ex "Supra"
+	 * @param nosTimeStandard          ex 1500
+	 * @param nosBottleAmountStandard  ex 1
+	 * @param nosStrengthStandard      ex 0.8
+	 * @param hp                       ex 220
+	 * @param currentWeight            ex 1400
+	 * @param topSpeed                 ex 250
+	 * @param idleSpeed                ex 1000
+	 * @param totalRPM                 ex 8000
+	 * @param totalGear                ex 6
+	 * @param upgradedGears            ex true
+	 * @param tireGripTimeStandard     ex 1000
+	 * @param tireGripStrengthStandard ex 0.5
+	 * @param tireGripAreaTop          ex 24
+	 * @param tireGripAreaBottom       ex 4
+	 * @param upgradeLVLs              ex {1, 2 ...}
+	 */
+	public CarRep(String name, int nosTimeStandard, int nosBottleAmountStandard, double nosStrengthStandard, double hp,
+			double currentWeight, double topSpeed, int idleSpeed, int totalRPM, int totalGear, boolean upgradedGears,
+			int tireGripTimeStandard, double tireGripStrengthStandard, double tireGripAreaTop,
+			double tireGripAreaBottom, int[] upgradeLVLs) {
+
 		this.name = name;
 		this.nosTimeStandard = nosTimeStandard;
 		this.nosBottleAmountStandard = nosBottleAmountStandard;
@@ -25,10 +58,14 @@ public class CarRepresentation {
 		this.hp = hp;
 		this.currentWeight = currentWeight;
 		this.topSpeed = topSpeed;
+		this.idleSpeed = idleSpeed;
 		this.totalRPM = totalRPM;
 		this.totalGear = totalGear;
 		this.upgradedGears = upgradedGears;
+		this.tireGripTimeStandard = tireGripTimeStandard;
 		this.tireGripStrengthStandard = tireGripStrengthStandard;
+		this.tireGripAreaTop = tireGripAreaTop;
+		this.tireGripAreaBottom = tireGripAreaBottom;
 		this.upgradeLVLs = upgradeLVLs;
 	}
 
@@ -88,6 +125,14 @@ public class CarRepresentation {
 		this.topSpeed = topSpeed;
 	}
 
+	public int getIdleSpeed() {
+		return idleSpeed;
+	}
+
+	public void setIdleSpeed(int idleSpeed) {
+		this.idleSpeed = idleSpeed;
+	}
+
 	public int getTotalRPM() {
 		return totalRPM;
 	}
@@ -112,12 +157,36 @@ public class CarRepresentation {
 		this.upgradedGears = upgradedGears;
 	}
 
+	public int getTireGripTimeStandard() {
+		return tireGripTimeStandard;
+	}
+
+	public void setTireGripTimeStandard(int tireGripTimeStandard) {
+		this.tireGripTimeStandard = tireGripTimeStandard;
+	}
+
 	public double getTireGripStrengthStandard() {
 		return tireGripStrengthStandard;
 	}
 
 	public void setTireGripStrengthStandard(double tireGripStrengthStandard) {
 		this.tireGripStrengthStandard = tireGripStrengthStandard;
+	}
+
+	public double getTireGripAreaTop() {
+		return tireGripAreaTop;
+	}
+
+	public void setTireGripAreaTop(double tireGripAreaTop) {
+		this.tireGripAreaTop = tireGripAreaTop;
+	}
+
+	public double getTireGripAreaBottom() {
+		return tireGripAreaBottom;
+	}
+
+	public void setTireGripAreaBottom(double tireGripAreaBottom) {
+		this.tireGripAreaBottom = tireGripAreaBottom;
 	}
 
 	public int[] getUpgradeLVLs() {
