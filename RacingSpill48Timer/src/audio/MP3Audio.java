74b4ff5d1fda7624e2f559dc6f6f095c1dd25f9b
jhoffis <jhoffis@jhoffis-Lap>
2019-09-25 18:23:42 +0200

    Better audio api

--- RacingSpill48Timer/src/audio/MP3Audio.java
+++ RacingSpill48Timer/src/audio/MP3Audio.java
@@ -1,74 +1,87 @@
 package audio;
 
-import adt.Audio;
-import handlers.GameHandler;
-import javafx.scene.media.AudioClip;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaPlayer.Status;
-import javafx.util.Duration;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-public class MP3Audio implements Audio{
-	private Media hit;
-	private MediaPlayer mediaPlayer;
-	private AudioClip loopPlayer;
+import org.apache.commons.io.FileUtils;
+
+import adt.Audio;
+import jaco.mp3.player.MP3Player;
+
+public class MP3Audio implements Audio {
+//	private Media hit;
+	private MP3Player mediaPlayer;
+//	private AudioClip loopPlayer;
 
 	public MP3Audio(String file) {
 		System.out.println("Finding file:");
-		System.out.println("\"" + file  +"\"");
-		System.out.println(MP3Audio.class.getResource(file + ".mp3").toString());
-		hit = new Media(MP3Audio.class.getResource(file + ".mp3").toString());
-		loopPlayer = new AudioClip(MP3Audio.class.getResource(file + ".mp3").toString());
-		mediaPlayer = new MediaPlayer(hit);
+		System.out.println("\"" + file + "\"");
+
+		InputStream input = getClass().getResourceAsStream(file + ".mp3");
+		File tempFile = null;
+		try {
+			tempFile = File.createTempFile("abc", null);
+			FileOutputStream out = new FileOutputStream(tempFile);
+			FileUtils.copyInputStreamToFile(input, tempFile);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		mediaPlayer = new MP3Player();
+		mediaPlayer.addToPlayList(tempFile);
+
 		setVolume(1);
 	}
 
 	@Override
 	public void setVolume(double factor) {
-		mediaPlayer.setVolume(GameHandler.getMasterVolume() * factor);		
+//		mediaPlayer.(GameHandler.volume * factor);		
 	}
 
 	@Override
 	public void setRate(double rate) {
-		mediaPlayer.setRate(rate);
+		// mediaPlayer.setRate(rate);
 	}
-	
+
 	public void play() {
 		mediaPlayer.play();
 	}
 
 	public void stop() {
-		mediaPlayer.stop();
+		if (isPlaying())
+			mediaPlayer.stop();
 	}
 
 	public void loop() {
 		mediaPlayer.play();
-		mediaPlayer.setOnEndOfMedia(new Runnable() {
-			public void run() {
-				mediaPlayer.seek(Duration.ZERO);
-			}
-		});
+//		mediaPlayer.setOnEndOfMedia(new Runnable() {
+//			public void run() {
+//				mediaPlayer.seek(Duration.ZERO);
+//			}
+//		});
 	}
 
 	public boolean isPlaying() {
-		return mediaPlayer.getStatus().equals(Status.PLAYING);
+		return !mediaPlayer.isPaused();
 	}
 
-	public Media getHit() {
-		return hit;
-	}
+//	public Media getHit() {
+//		return hit;
+//	}
+//
+//	public void setHit(Media hit) {
+//		this.hit = hit;
+//	}
+//
+//	public MediaPlayer getMediaPlayer() {
+//		return mediaPlayer;
+//	}
+//
+//	public void setMediaPlayer(MediaPlayer mediaPlayer) {
+//		this.mediaPlayer = mediaPlayer;
+//	}
 
-	public void setHit(Media hit) {
-		this.hit = hit;
-	}
-
-	public MediaPlayer getMediaPlayer() {
-		return mediaPlayer;
-	}
-
-	public void setMediaPlayer(MediaPlayer mediaPlayer) {
-		this.mediaPlayer = mediaPlayer;
-	}
-
-	
 }
